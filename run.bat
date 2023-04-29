ab962495068c5d796558a58fe5d2f95f8c93248b
DOSmile <kristoffdosmile1@gmail.com>
2023-04-29 13:21:02 -0400

    Revert "Update run.bat"
    This reverts commit 8dcb323a6e514987df344da2c78a41c57ebc3fa0.

--- run.bat
+++ run.bat
@@ -1,28 +1,18 @@
 @echo off
-echo Checking Python 3.10 installation...
 
-set "python_exe=C:\Python310\python.exe"
-
-if not exist "%python_exe%" (
-    echo Python 3.10 is not found. Installing Python 3.10 to the default directory on the C: drive...
-    powershell -Command "Invoke-WebRequest -Uri 'https://www.python.org/ftp/python/3.10.0/python-3.10.0-amd64.exe' -OutFile '%TEMP%\python-3.10.0-amd64.exe'"
-    %TEMP%\python-3.10.0-amd64.exe /quiet InstallAllUsers=1 PrependPath=1 TargetDir="C:\Python310"
-    echo Python 3.10 installed.
-) else (
-    for /f "delims=" %%v in ('"%python_exe%" --version') do set "py_version=%%v"
-    echo %py_version% found at %python_exe%
-    if not %py_version:~0,8% == Python 3. (
-        echo Please manually install Python 3.10 to the default directory on the C: drive.
-        pause
-        exit
-    )
+where python 2>nul || (
+    echo Python 3.10 is not installed. Downloading and installing...
+    powershell -Command "(New-Object Net.WebClient).DownloadFile('https://www.python.org/ftp/python/3.10.0/python-3.10.0-amd64.exe', 'python-3.10.0-amd64.exe')"
+    .\python-3.10.0-amd64.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
+    echo Installation complete.
 )
 
-echo Installing required packages...
-"%python_exe%" -m pip install --upgrade pip
-"%python_exe%" -m pip install requests
+echo Installing requests module if not installed...
+python -m pip install --upgrade pip
+python -m pip install requests
 
-echo Running the Python script...
-"%python_exe%" mod_downloader.py
+echo Running the mod_downloader.py script...
+python .\mod_downloader.py
 
+echo Finished.
 pause
