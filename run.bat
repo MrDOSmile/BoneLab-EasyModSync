1361532ac7eb58a4847679e3a8633129ad0af764
DOSmile <kristoffdosmile1@gmail.com>
2023-04-29 13:25:16 -0400

    Update run.bat
    For some reason, the first approach wasn't working. I've updated it to use 2 approaches for finding the python 3.10 install. If it can't find it after that, it should download and install it.

--- run.bat
+++ run.bat
@@ -1,6 +1,13 @@
 @echo off
 
+set "python_exe=python"
+
 where python 2>nul || (
+    echo Python not found in the system's PATH. Checking the default installation path...
+    set "python_exe=C:\Python310\python.exe"
+)
+
+if not exist "%python_exe%" (
     echo Python 3.10 is not installed. Downloading and installing...
     powershell -Command "(New-Object Net.WebClient).DownloadFile('https://www.python.org/ftp/python/3.10.0/python-3.10.0-amd64.exe', 'python-3.10.0-amd64.exe')"
     .\python-3.10.0-amd64.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
@@ -8,11 +15,11 @@ where python 2>nul || (
 )
 
 echo Installing requests module if not installed...
-python -m pip install --upgrade pip
-python -m pip install requests
+"%python_exe%" -m pip install --upgrade pip
+"%python_exe%" -m pip install requests
 
 echo Running the mod_downloader.py script...
-python .\mod_downloader.py
+"%python_exe%" .\mod_downloader.py
 
 echo Finished.
 pause
